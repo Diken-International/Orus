bae509b9ecbb7fb2ab2ec96312f6ce3840de3685
Carmona Colin Jose Alb <al221411868@gmail.com>
2021-04-21 23:14:16 -0500

    JWT Token

---
+++ app/Http/Middleware/JWT.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use JWTAuth;
+
+class JWT
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        JWTAuth::parseToken()->authenticate();
+
+        $request->current_user = Auth::user();
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,22 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::group([
+    'prefix' => 'auth',
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
+
+Route::get('/name', function(){
+    return response()->json(['msj'=>"ok"]);
 });

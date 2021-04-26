c216aea5196f5cca253048a226df9d13650bdedc
Carmona Colin Jose Alb <al221411868@gmail.com>
2021-04-26 13:35:43 -0500

    add

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ Route::group(['middleware' => ['jwt']], function () {
 
     Route::get('clients','ClientsController@index')->name('clients.index');
     Route::post('client','ClientsController@store')->name('clients.store');
-    Route::put('clients/{template_id}', 'ClientsController@update')->name('clients.update');
-    Route::delete('clients/{template_id}', 'ClientsController@destroy')->name('clients.destroy');
+    Route::put('clients/{client_id}', 'ClientsController@update')->name('clients.update');
+    Route::delete('clients/{client_id}', 'ClientsController@destroy')->name('clients.destroy');
 
 });
